947c9a5e1c4f070fca5c6178c61d97de71e632f2
WNHS10494\kenta <kentarou20020726@gmail.com>
2024-05-10 18:49:41 +0900

    スタジオオブジェクトマネージャーのスクリプトを作成

---
+++ GameAward2024/Assets/ManagerScript/GameScene/StudioObjects/StudioObjectManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace GameScene
+{
+	public class StudioObjectManager : MonoBehaviour
+	{
+		public Camera m_mainCamera;
+		public Light m_directionLight;
+		public Volume m_volume;
+	}
+}
