96abd43c573cfc17dae34a2e8f1f6aa3a0f4ef36
WNHS10494\kenta <kentarou20020726@gmail.com>
2024-05-10 18:48:46 +0900

    ゲームマネージャーのスクリプトを作成

---
+++ GameAward2024/Assets/ManagerScript/GameScene/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameScene
+{
+	public class GameManager : MonoBehaviour
+	{
+		//public GameTimer m_gameTimer;
+	}
+}
