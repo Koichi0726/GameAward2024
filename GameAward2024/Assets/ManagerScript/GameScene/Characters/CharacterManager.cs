cb17a0823bebf0cc44495b28ef0cf6c519057a72
WNHS10494\kenta <kentarou20020726@gmail.com>
2024-05-10 18:50:01 +0900

    キャラクターマネージャーのスクリプトを作成

---
+++ GameAward2024/Assets/ManagerScript/GameScene/Characters/CharacterManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameScene
+{
+	public class CharacterManager : MonoBehaviour
+	{
+		//public Player m_player;
+		//public Enemy m_enemy;
+	}
+}
