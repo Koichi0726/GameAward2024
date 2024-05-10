19e1e7802e8fafe7d19a3debbda93672696142ce
WNHS10494\kenta <kentarou20020726@gmail.com>
2024-05-10 18:49:05 +0900

    UIマネージャーのスクリプトを作成

---
+++ GameAward2024/Assets/ManagerScript/GameScene/UI/UIManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameScene
+{
+	public class UIManager : MonoBehaviour
+	{
+		public PlayerGaugeController m_playerGaugeController;
+		public TimeGaugeController m_timeGaugeController;
+	}
+}
