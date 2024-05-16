0bdb7bce43eb98f8dcc62a578ce147c0549f59ae
WNHS10494\kenta <kentarou20020726@gmail.com>
2024-05-16 15:38:49 +0900

    プレイヤーに追従する仕組みを行列計算から内積・外積計算に変更

--- GameAward2024/Assets/Script/GameScene/StudioObjects/FollowPlayer.cs
+++ GameAward2024/Assets/Script/GameScene/StudioObjects/FollowPlayer.cs
@@ -5,34 +5,61 @@ using GameScene;
 
 public class FollowPlayer : MonoBehaviour
 {
-	Transform playerTrans;
-	Matrix4x4 worldMat;
+	const float HALF_DEGREE = 180.0f;
+
+	[SerializeField]
+	Vector3 m_offset = new Vector3(0.0f, 0.5f, -2.5f);
+	Transform m_playerTrans;
+	Transform m_enemyTrans;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		//--- プレイヤーの情報を取得
+		//--- プレイヤー・敵の情報を取得
 		CharacterManager characterManager = ManagerContainer.GetManagerContainer().m_characterManager;
-		playerTrans = characterManager.m_player;
-		
-		// オブジェクトのワールド行列を取得
-		worldMat = this.transform.localToWorldMatrix;
+		m_playerTrans = characterManager.m_player;
+		m_enemyTrans = characterManager.m_enemy;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		// プレイヤーのワールド行列
-		Matrix4x4 playerWorldMat = playerTrans.localToWorldMatrix;
-		// プレイヤーの行列に自身の行列を適用
-		Matrix4x4 newWorldMat = playerWorldMat * worldMat;
+		//--- プレイヤー・敵の座標
+		Vector3 playerPos = m_playerTrans.position;
+		Vector3 enemyPos  = m_enemyTrans.position;
 
-		//--- 移動成分を取り出す
-		Vector3 pos = new Vector3(newWorldMat.m03, newWorldMat.m13, newWorldMat.m23);
-		this.transform.position = pos;
+		// 角度を計算
+		float angle = CalcToEnemyAngleY(enemyPos - playerPos);
 
-		//--- 回転成分を取り出す
-		Quaternion rot = newWorldMat.rotation;
-		this.transform.rotation = rot;
+		//--- オフセットに回転を適用
+		Quaternion quaternion = Quaternion.Euler(0.0f, angle, 0.0f);
+		Vector3 newOffset = quaternion * m_offset;
+
+		//--- 座標・回転を指定
+		transform.position = playerPos + newOffset;
+		transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+	}
+
+	/// <summary>
+	/// 敵へ向かうベクトルのY軸の角度を求める
+	/// </summary>
+	/// <param name="vToEnemy">プレイヤーから敵へ向かうベクトル</param>
+	/// <returns>敵へ向かうベクトルのY軸の角度</returns>
+	float CalcToEnemyAngleY(Vector3 vToEnemy)
+	{
+		vToEnemy.y = 0.0f;		// yの移動成分を無視
+		vToEnemy.Normalize();	// ベクトルを正規化
+
+		//--- 敵へのベクトルとZ方向ベクトルの成す角を求める
+		float dot = Vector3.Dot(Vector3.forward, vToEnemy);
+		float angle = Mathf.Acos(dot);
+		angle = angle * (HALF_DEGREE / Mathf.PI);
+
+		//--- 左右判定によって-180.0～180.0に変換
+		Vector3 cross = Vector3.Cross(Vector3.forward, vToEnemy);
+		// Z方向ベクトルから見て、左側に位置する場合は[* -1.0]
+		if (cross.y < 0.0f) angle = -angle;
+
+		return angle;
 	}
 }
