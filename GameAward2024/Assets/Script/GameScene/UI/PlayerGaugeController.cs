18247196cf178c0e618821bcfdb1a6e1b4284c45
iwatakouki <2002gg1001@gmail.com>
2024-05-17 14:56:18 +0900

    コメント追記

--- GameAward2024/Assets/Script/GameScene/UI/PlayerGaugeController.cs
+++ GameAward2024/Assets/Script/GameScene/UI/PlayerGaugeController.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class PlayerGaugeController : MonoBehaviour
 {
-	[SerializeField]
-	Material m_gaugeMaterial;
-	float m_gaugeValue = 0.4f;
+	[SerializeField] Material m_gaugeMaterial;	//ゲージのマテリアル
+	float m_gaugeValue = 0.4f;	//マテリアルに設定する数値
 
     // Start is called before the first frame update
     void Start()
     {
+		//初期化
 		m_gaugeValue = PlayerActionControler.ActionValue / 100.0f;
 	}
 
     // Update is called once per frame
     void FixedUpdate()
     {
+		//マテリアルが設定されていなかった時終了する
 		if (m_gaugeMaterial == null) return;
 
+		//設定する数値を取得して計算
 		m_gaugeValue = PlayerActionControler.ActionValue / 100.0f;
 
+		//マテリアルに適応
 		m_gaugeMaterial.SetFloat("_GaugeValue", m_gaugeValue);
 	}
 }
