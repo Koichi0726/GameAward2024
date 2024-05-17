2d176b30bfed1927af8fbb1a5a79df3ccfcf1dc5
WNHS10494\kenta <kentarou20020726@gmail.com>
2024-05-18 03:33:59 +0900

    各敵のデータを持つScriptableObjectを作成

---
+++ GameAward2024/Assets/ScriptableObject/EnemySettingData/EnemySettingDataBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemySettingDataBase", menuName = "ScriptableObjects/EnemySettingDataBase")]
+public class EnemySettingDataBase : ScriptableObject
+{
+
+}
