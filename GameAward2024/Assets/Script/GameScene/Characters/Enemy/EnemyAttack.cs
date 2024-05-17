38b0e43f9f2a25f121adcc11bb82616cb06bb847
yosiki <amayotsoy@gmail.com>
2024-05-17 18:06:30 +0900

    スクリプトの修正とマテリアルを追加しました

--- GameAward2024/Assets/Script/GameScene/Characters/Enemy/EnemyAttack.cs
+++ GameAward2024/Assets/Script/GameScene/Characters/Enemy/EnemyAttack.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    MeshRenderer meshRenderer;
-    public Material[] Materials1;   //変更前のマテリアル
-    public Material[] Materials2;   //変更後のマテリアル
-    public GameObject Object;       //敵の投擲するオブジェクト設定用の変数
-    public float AttackRate = 5.0f; //敵の攻撃間隔設定用の変数
-    private bool AttackFlag = false;        //攻撃時のカラー変更用のフラグ
+    [SerializeField] MeshRenderer m_meshRenderer;
+    [Header("デバックで中身を確認する用なので空で大丈夫です"),SerializeField] Material[] m_Materials1;   //変更前のマテリアル
+    [Space(10),SerializeField] Material[] m_Materials2;   //変更後のマテリアル
+    [SerializeField] GameObject m_Object;       //敵の投擲するオブジェクト設定用の変数
+    [SerializeField] float m_AttackRate = 5.0f; //敵の攻撃間隔設定用の変数
+    private bool m_AttackFlag = false;        //攻撃時のカラー変更用のフラグ
 
     // Start is called before the first frame update
     void Start()
     {
-        meshRenderer = GetComponent<MeshRenderer>();
+        //m_meshRenderer = GetComponent<MeshRenderer>();
         //CreateObjを3.5秒後に呼び出し、以降は AttackRate 秒毎に実行
-
-        InvokeRepeating(nameof(CreateObj), 3.5f, AttackRate);
+        m_Materials1 = m_meshRenderer.sharedMaterials;
+        InvokeRepeating(nameof(CreateObj), 3.5f, m_AttackRate);
     }
 
     void Update()
     {
-        //�A�^�b�N�t���O��true�ɂȂ�����}�e���A���̃J���[��ύX���邽�߂̃R���[�`�����N������
-        if (AttackFlag == true)
+        //アタックフラグがtrueになったら敵のマテリアル変更用のコルーチンを呼び出す
+        if (m_AttackFlag == true)
         {
             StartCoroutine("ATTACKFLAG");
         }
@@ -31,21 +31,21 @@ public class EnemyAttack : MonoBehaviour
 
     void CreateObj()　// 敵の投擲するオブジェクトを生成する
     {
-        meshRenderer.materials = Materials2;
+        m_meshRenderer.materials = m_Materials2;
 
         //Instantiate( 生成するオブジェクト,  場所, 回転 );
         //現在はエネミーの頭上に生成するようにしています
         //Instantiate(Object, new Vector3(this.transform.localPosition.x, this.transform.localPosition.y + 2, this.transform.localPosition.z), Quaternion.identity);
 
         Debug.Log("敵からの攻撃!!");
-        AttackFlag = true;
+        m_AttackFlag = true;
     }
 
     IEnumerator ATTACKFLAG()
     {
 
         yield return new WaitForSeconds(1.0f);  //処理の遅延
-        meshRenderer.materials = Materials1;
-        AttackFlag = false;
+        m_meshRenderer.materials = m_Materials1;
+        m_AttackFlag = false;
     }
 }
