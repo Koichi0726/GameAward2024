034a49004c6be94756626bf083e01e19f8bc4cd3
WNHS10494\kenta <kentarou20020726@gmail.com>
2024-05-28 21:26:51 +0900

    弾を作成する関数を作成

--- GameAward2024/Assets/ManagerScript/GameScene/Bullets/BulletManager.cs
+++ GameAward2024/Assets/ManagerScript/GameScene/Bullets/BulletManager.cs
@@ -6,5 +6,17 @@ namespace GameScene
 {
 	public class BulletManager : MonoBehaviour
 	{
+		/// <summary>
+		/// 弾を作成
+		/// </summary>
+		/// <typeparam name="T">BulletBaseを継承したクラスに限定</typeparam>
+		/// <param name="bulletPrefab">弾のプレハブ</param>
+		/// <returns>作成した弾への参照</returns>
+		public T CreateBullet<T>(BulletBase bulletPrefab) where T : BulletBase
+		{
+			BulletBase bullet = Instantiate(bulletPrefab);	// 弾を作成
+			bullet.transform.SetParent(this.transform);		// 親をBulletManagerに設定
+			return bullet as T;	// 目的のクラスへキャスト
+		}
 	}
 }
