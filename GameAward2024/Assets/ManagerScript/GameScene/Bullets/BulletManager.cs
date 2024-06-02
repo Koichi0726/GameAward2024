559c4b82f911b8ce7ff4249d111e01e771bff881
yosiki <amayotsoy@gmail.com>
2024-06-02 18:53:46 +0900

    弾の発射の修正とモデルの入れかえ、アニメーションの適応

--- GameAward2024/Assets/ManagerScript/GameScene/Bullets/BulletManager.cs
+++ GameAward2024/Assets/ManagerScript/GameScene/Bullets/BulletManager.cs
@@ -12,9 +12,10 @@ namespace GameScene
 		/// <typeparam name="T">BulletBaseを継承したクラスに限定</typeparam>
 		/// <param name="bulletPrefab">弾のプレハブ</param>
 		/// <returns>作成した弾への参照</returns>
-		public T CreateBullet<T>(BulletBase bulletPrefab) where T : BulletBase
+		public T CreateBullet<T>(BulletBase bulletPrefab , Vector3 vector3) where T : BulletBase
 		{
-			BulletBase bullet = Instantiate(bulletPrefab);	// 弾を作成
+
+			BulletBase bullet = Instantiate(bulletPrefab,vector3 ,Quaternion.identity);	// 弾を作成
 			bullet.transform.SetParent(this.transform);		// 親をBulletManagerに設定
 			return bullet as T;	// 目的のクラスへキャスト
 		}
