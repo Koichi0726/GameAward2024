9d402b7247802221d304b6e9ef761d26ab6cdf4d
WNHS10494\kenta <kentarou20020726@gmail.com>
2024-06-28 17:12:54 +0900

    不要な処理を削除

--- GameAward2024/Assets/Script/GameScene/Characters/Player/PlayerMove.cs
+++ GameAward2024/Assets/Script/GameScene/Characters/Player/PlayerMove.cs
@@ -8,108 +8,119 @@ public class PlayerMove : MonoBehaviour
 {
 	const float MAX_DEGREE = Mathf.PI * 2.0f * Mathf.Rad2Deg;
 
-	// プレイヤーデータの取得用変数
-	PlayerData m_playerData;
 
-	// プレイヤーパラメータ係数
-	PlayerParamCoefficient m_paramCoefficient;
+	PlayerData m_playerData;    // プレイヤーデータの取得用変数
+	PlayerParamCoefficient m_paramCoefficient;  // プレイヤーパラメータ係数
 
-	PlayerActionControler m_playerActionControler;
+	Transform m_enemyTrans; // 目標（座標を使用）
 
-	// 目標（座標を使用）
-	Transform m_enemyTrans;
+	bool m_isDash = false;	// 走るフラグ
+	bool m_isAvoid = false;	// 回避フラグ
+	
+	Vector2 m_moveDirect = new Vector2();	// 移動する方向
+	Vector2 m_period = new Vector2();		// 円運動周期
 
-    // 走るフラグ
-    bool m_isDash = false;
-
-    // 前フレームのワールド座標
-    Vector3 m_prePos;
-
-    // プレイヤーのポジション
-    Vector3 m_pos;
-
-	// 実際に使用する円運動周期
-	Vector2 m_period = new Vector2();
-
-    void Start()
+	void Start()
     {
 		CharacterManager characterManager = ManagerContainer.instance.characterManager;
         m_playerData = characterManager.playerData;
 		m_paramCoefficient = characterManager.buffDebuffHandler.m_paramCoefficient;
-		m_playerActionControler = characterManager.playerActionController;
 		m_enemyTrans = characterManager.enemyTrans;
-		m_pos = m_prePos = transform.position;
-    }
+		
+	}
 
     void FixedUpdate()
-    {
-        // 次のUpdateで使うための前フレーム位置更新
-        m_prePos = m_pos;
+	{
+		if (m_isAvoid) Avoid();
+		else Move();
+	}
 
-        //--- 左右左右の移動がなければ終了する
-        if (Mathf.Approximately(m_period.x, 0.0f) && Mathf.Approximately(m_period.y, 0.0f))
-			return;
+	/// <summary>
+	/// 移動処理
+	/// </summary>
+	void Move()
+	{
+		//--- 円運動周期を計算
+		m_moveDirect.Normalize();
+		m_period.x = -m_moveDirect.x * m_playerData.HORIZONTAL_MOVE_SPEED;
+		m_period.y =  m_moveDirect.y * m_playerData.VERTICAL_MOVE_SPEED;
+		if (m_isDash) m_period /= m_playerData.DASH_MOVE_SPEED_MULTIPLIER;
 
-		// ダッシュ時
-        if (m_isDash)	m_period /= m_playerData.DASH_MOVE_SPEED_MULTIPLIER;
-
-		// 移動速度を適用
+		//--- 移動速度を適用
 		m_period /= m_paramCoefficient.m_moveSpeed;
 		m_period *= m_paramCoefficient.m_moveDirect;
 
 		//--- 移動処理
-		if (m_period.x != 0.0f) PlayerCircularRotation(m_period.x, this.transform.up);
-        if (m_period.y != 0.0f) PlayerCircularRotation(m_period.y, this.transform.right);
-        transform.position = m_pos;
-		m_paramCoefficient.m_moveDirect = m_period.normalized;
+		CircularRotation(m_period.x, this.transform.up);
+		CircularRotation(m_period.y, this.transform.right);
+	}
 
-		// 移動量をリセット
-		m_period.x = m_period.y = 0.0f;
-    }
+	/// <summary>
+	/// 回避処理
+	/// </summary>
+	void Avoid()
+	{
+		//--- 移動処理
+		CircularRotation(m_period.x, this.transform.up);
+		CircularRotation(m_period.y, this.transform.right);
 
-    /// <summary>
-    /// 上入力したときの処理関数
-    /// </summary>
-    public void OnMoveUp()
-    {
-		m_period.y = m_playerData.VERTICAL_MOVE_SPEED;
-        ActionEntry();
-    }
+		// 加速させていく
+		m_period *= m_playerData.AVOID_ANCE_MULTIPLIER;
 
-    /// <summary>
-    /// 下入力したときの処理関数
-    /// </summary>
-    public void OnMoveDown()
-    {
-		m_period.y = -m_playerData.VERTICAL_MOVE_SPEED;
-        ActionEntry();
-    }
+		//--- 回避の限界値で停止させる
+		if (Mathf.Abs(m_period.x) >= m_playerData.AVOID_RIMIT_VALUE)
+			m_period.x = 0.0f;
+		if (Mathf.Abs(m_period.y) >= m_playerData.AVOID_RIMIT_VALUE)
+			m_period.y = 0.0f;
 
-    /// <summary>
-    /// 左入力したときの処理関数
-    /// </summary>
-    public void OnMoveLeft()
-    {
-		m_period.x = m_playerData.HORIZONTAL_MOVE_SPEED;
-        ActionEntry();
-    }
+		// 回避の終了を判定
+		m_isAvoid = (m_period != Vector2.zero);
+	}
 
-    /// <summary>
-    /// 右入力したときの処理関数
-    /// </summary>
-    public void OnMoveRight()
-    {
-		m_period.x = -m_playerData.HORIZONTAL_MOVE_SPEED;
-        ActionEntry();
-    }
+	/// <summary>
+	/// 敵を中心に指定の軸で回転
+	/// </summary>
+	/// <param name="p">回転量</param>
+	/// <param name="axis">回転軸</param>
+	public void CircularRotation(float p, Vector3 axis)
+	{
+		if (Mathf.Approximately(p, 0.0f)) return;
 
+		//--- 変数宣言
+		Vector3 center = m_enemyTrans.position;   //回転の中心
+		var angleAxis = Quaternion.AngleAxis(MAX_DEGREE / p * Time.deltaTime, axis);     //クオータニオンの計算
+
+		// 移動先を算出
+		Vector3 pos = transform.position;
+		pos -= center;
+		pos = angleAxis * pos;
+		pos += center;
+		transform.position = pos;
+	}
+
+	/// <summary>
+	/// 方向転換処理
+	/// </summary>
+	public void OnChangeDirect(InputAction.CallbackContext context)
+	{
+		if (!context.performed) return;
+
+		Vector2 axis = context.ReadValue<Vector2>();
+		m_moveDirect = axis.normalized;
+
+		//--- タイマーのカウントを始める
+		GameTimer gameTimer = ManagerContainer.instance.gameManager.gameTimer;
+		if (gameTimer.m_isStart) return;
+		gameTimer.StartCount();
+	}
+	
     /// <summary>
     /// 走り始めた時の処理関数
     /// </summary>
     public void OnDashStart(InputAction.CallbackContext context)
     {
-        if (!context.started) return;
-        m_isDash = true;
+       if (!context.performed) return;
+		m_isDash = true;
     }
 
     /// <summary>
@@ -121,30 +132,18 @@ public class PlayerMove : MonoBehaviour
         m_isDash = false;
     }
 
-    /// <summary>
-    /// 移動方法に応じてアクションを登録する関数
-    /// </summary>
-    private void ActionEntry()
-    {
-        if(!m_isDash)
-        {
-			m_playerActionControler.AddAction(PlayerData.E_PLAYER_ACTION.MOVE);
-        }
-        else
-        {
-			m_playerActionControler.AddAction(PlayerData.E_PLAYER_ACTION.DASH);
-        }
-    }
+	public void OnAvoid(InputAction.CallbackContext context)
+	{
+		if (!context.performed) return;
+		if (m_isAvoid) return;
 
-    public void PlayerCircularRotation(float p, Vector3 axis)
-    {
-        //--- 変数宣言
-        Vector3 center = m_enemyTrans.position;   //回転の中心
-        var angleAxis = Quaternion.AngleAxis(MAX_DEGREE / p * Time.deltaTime, axis);     //クオータニオンの計算
+		//--- 回避する方向を計算
+		m_moveDirect.Normalize();
+		m_period.x = -m_playerData.AVOID_START_VALUE * m_moveDirect.x;
+		m_period.y =  m_playerData.AVOID_START_VALUE * m_moveDirect.y;
+		m_period *= m_paramCoefficient.m_moveDirect;
+		m_period /= m_paramCoefficient.m_moveSpeed;
 
-        // 移動先を算出
-        m_pos -= center;
-        m_pos = angleAxis * m_pos;
-        m_pos += center;
-    }
+		m_isAvoid = true;   // 回避フラグを立てる
+	}
 }
