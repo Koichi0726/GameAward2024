3282e0ba9bfdf06293cee5e37cbc81d3d3acdfa0
WNHS10494\kenta <kentarou20020726@gmail.com>
2024-06-28 17:13:12 +0900

    ゲーム終了判定処理を変更

--- GameAward2024/Assets/Script/GameScene/StudioObjects/GameObserver.cs
+++ GameAward2024/Assets/Script/GameScene/StudioObjects/GameObserver.cs
@@ -9,17 +9,19 @@ public class GameObserver : MonoBehaviour
 	SceneData m_sceneData;
 
     GameTimer m_gameTimer;
-	PlayerGaugeController m_playerGaugeController;
+	PlayerControler m_player;
+	EnemyBase m_enemy;
     FadeController m_fadeController;
        
     void Start()
     {
 		ManagerContainer managerContainer = ManagerContainer.instance;
         m_gameTimer = managerContainer.gameManager.gameTimer;
-	 	m_playerGaugeController = managerContainer.uiManager.playerGaugeController;
-
+	 	CharacterManager characterManager = managerContainer.characterManager;
+		m_player = characterManager.playerController;
+		m_enemy = characterManager.enemyData;
 		m_fadeController = FadeController.instance;
-    }
+	}
 
     void Update()
     {
@@ -27,12 +29,25 @@ public class GameObserver : MonoBehaviour
 		bool isfade = (m_fadeController.m_fadeState != FadeController.E_FADE_STATE.NONE);
 		if (isfade) return;
 
-		//--- タイムアップの場合→ゲームクリア
+		//--- タイムアップの場合→ゲームオーバー
 		if (m_gameTimer.IsTimeUp())
-			m_fadeController.FadeSceneLoad(m_sceneData.GetSceneName(SceneData.E_SCENE_KIND.GAME_CLEAR_SCENE));
-
-		//--- ゲージが0or100の範囲を超えた場合→ゲームオーバー
-		if(m_playerGaugeController.IsOutOfGaugeValue())
+		{
 			m_fadeController.FadeSceneLoad(m_sceneData.GetSceneName(SceneData.E_SCENE_KIND.GAME_OVER_SCENE));
+			return;
+		}
+	
+		//--- プレイヤーが死亡した場合→ゲームオーバー
+		if(m_player.IsDead())
+		{
+			m_fadeController.FadeSceneLoad(m_sceneData.GetSceneName(SceneData.E_SCENE_KIND.GAME_OVER_SCENE));
+			return;
+		}
+
+		//--- 敵が死亡した場合→ゲームクリア
+		if (m_enemy.IsDead())
+		{
+			m_fadeController.FadeSceneLoad(m_sceneData.GetSceneName(SceneData.E_SCENE_KIND.GAME_CLEAR_SCENE));
+			return;
+		}
 	}
 }
